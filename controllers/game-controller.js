8f5607ec9d0b8509aa22e165a98849b8e6cc840c
Amir Ashlag <ashlag.amir@gmail.com>
2023-02-15 16:38:09 +0200

    a

--- controllers/game-controller.js
+++ controllers/game-controller.js
@@ -1,48 +1,62 @@
 const axios = require('axios');
 
-function getGamesByDate(req, res) {
-  console.log(req.params);
 
-  var today = new Date();
-  var dd = today.getDate();
+function getGamesByDate(req,res){
+    console.log(req.params)
 
-  var mm = today.getMonth() + 1;
-  var yyyy = today.getFullYear();
-  if (dd < 10) {
-    dd = '0' + dd;
-  }
+    var today = new Date();
+    var dd = today.getDate();
 
-  if (mm < 10) {
-    mm = '0' + mm;
-  }
-  console.log(today);
-  if (Object.keys(req.params).length > 0) {
-    today = req.params.date;
-  } else {
-    today = yyyy + '-' + mm + '-' + dd;
-  }
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
 
-  const axios = require('axios');
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+    
+    console.log(today);
+    if (Object.keys(req.params).length === 0){
+        today = yyyy + '-' + mm + '-' + dd;
+    } else {
+        today = new Date(req.params.date)
+         var dd = today.getDate();
 
-  const options = {
-    method: 'GET',
-    url: 'https://api-nba-v1.p.rapidapi.com/games',
-    params: { date: `${today}` },
-    headers: {
-      'X-RapidAPI-Key': '9bb573e2a6msh68425984afeb9f2p16011cjsn8273c5377197',
-      'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com',
-    },
-  };
+         var mm = today.getMonth() + 1;
+         var yyyy = today.getFullYear();
+         if (dd < 10) {
+           dd = '0' + dd;
+         }
 
-  axios
-    .request(options)
-    .then(function (response) {
-      console.log(response.data);
-      res.send(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+         if (mm < 10) {
+           mm = '0' + mm;
+         }
+         today = yyyy + '-' + mm + '-' + dd;
+    }
+
+    const axios = require('axios');
+
+    const options = {
+      method: 'GET',
+      url: 'https://api-nba-v1.p.rapidapi.com/games',
+      params: { date: `${today}` },
+      headers: {
+        'X-RapidAPI-Key': '9bb573e2a6msh68425984afeb9f2p16011cjsn8273c5377197',
+        'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com',
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        res.send(response.data)
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
 }
 
 module.exports = { getGamesByDate };
